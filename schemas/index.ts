de77e671626f4b63ec5d72f3f8a9879b7489835b
mkijowski <maciej.kijowski@felgdent.com>
2025-02-16 17:38:52 +0100

    firt commit

---
+++ schemas/index.ts
@@ -0,0 +1,8 @@
+import user from "./users";
+import account from "./account";
+import booking from "./booking";
+import hotelRoom from "./hotelRoom";
+import review from "./review";
+import verificationToken from "./verificationToken";
+
+export const schemas = [user, account, booking, hotelRoom, review, verificationToken];
