61b69b38bf0dbde47dd4cc7824fca9b5a9f793a2
mkijowski <maciej.kijowski@felgdent.com>
2025-05-23 22:06:24 +0200

    error fix

--- app/(web)/error.tsx
+++ app/(web)/error.tsx
@@ -1,6 +1,7 @@
 "use client";
 
-export default function Error({ reset }: { error: Error & { digest?: string }; reset: () => void }) {
+export default function Error({ error, reset }: { error: Error & { digest?: string }; reset: () => void }) {
+    console.error("Error: ", error);
     return (
         <div className="container mx-auto">
             <h2 className="font-heading text-red-800 mb-10">Something went wrong!</h2>
